86b93f58c6af7c430a34f11a9c0e73ca6ac21c34
nataliaoka <nataliaoka@hotmail.com>
2019-08-03 14:39:33 -0300

    correcao dos erros de formatação apontados pelo check style e findBugs

--- src/main/java/com/testService/TestController.java
+++ src/main/java/com/testService/TestController.java
@@ -4,10 +4,19 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+* Exemplo básico de um comentário em JavaDoc com mais de uma linha.
+* @author  natalia oka
+* @version 1.0
+* @since   2019-08-03
+*/
 @RestController
 @CrossOrigin
 public class TestController {
-
+   /**
+   * Method testController.
+   * @return string This returns a text.
+   */
 	@GetMapping("/home")
 	public String homeRequest() {
 		return "Hello World!";
--- src/main/java/com/testService/TestServiceApplication.java
+++ src/main/java/com/testService/TestServiceApplication.java
@@ -1,19 +1,30 @@
 package com.testService;
 
-import com.testService.TestController;
-
+/**
+ * Exemplo básico de um comentário em JavaDoc.
+*/
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
+/**
+* Exemplo básico de um comentário em JavaDoc com mais de uma linha.
+*/
 @SpringBootApplication
 @ComponentScan(basePackageClasses = { TestController.class })
 @EnableAutoConfiguration
 public class TestServiceApplication {
-
+/**
+* Exemplo básico de um comentário em JavaDoc com mais de uma linha.
+*/
 	private String bar = new String("bar");
+
 	public static void main(String[] args) {
+	/**
+	* Exemplo básico de um comentário 
+	* em JavaDoc com mais de uma linha.
+	*/
 		SpringApplication.run(TestServiceApplication.class, args);
 		System.exit(0);
 	}
